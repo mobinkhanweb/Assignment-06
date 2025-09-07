2b0191aaf32b677d5f6f341d10307105458088bd
mobinkhanweb <mobinkhanweb@gmail.com>
2025-09-07 14:44:22 +0600

    added catagories with api/js

--- check.js
+++ check.js
@@ -1,4 +1,20 @@
-// console.log("added the js");
+// Use Strict mode
+"use strict";
+
+// navbar functionality for mobile
+const mobileNav = document.getElementById("mobileNav");
+const overlay = document.getElementById("overlay");
+const showNav = () => {
+    mobileNav.classList.remove("right-[-200px]");
+    mobileNav.classList.add("right-0");
+    overlay.classList.remove("hidden");
+};
+const closeNav = () => {
+    mobileNav.classList.add("right-[-200px]");
+    mobileNav.classList.remove("right-0");
+    overlay.classList.add("hidden");
+};
+
 // Main Features
 const categoriesContainer = document.querySelector("#categoriesContainer");
 const treesCardContainer = document.querySelector("#treesCardContainer");
