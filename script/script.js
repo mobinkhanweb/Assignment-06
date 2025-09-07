8ec1033405ecb478640de416f15ddde4dea7287b
mobinkhanweb <mobinkhanweb@gmail.com>
2025-09-07 17:19:14 +0600

    added cart js

--- script/script.js
+++ script/script.js
@@ -1,7 +1,24 @@
-// console.log("added the js");
+// Use Strict mode
+"use strict";
+
+// navbar functionality for mobile
+const mobileNav = document.getElementById("mobileNav");
+const overlay = document.getElementById("overlay");
+const showNav = () => {
+    mobileNav.classList.remove("right-[-200px]");
+    mobileNav.classList.add("right-0");
+    overlay.classList.remove("hidden");
+};
+const closeNav = () => {
+    mobileNav.classList.add("right-[-200px]");
+    mobileNav.classList.remove("right-0");
+    overlay.classList.add("hidden");
+};
+
 // Main Features
 const categoriesContainer = document.querySelector("#categoriesContainer");
 const treesCardContainer = document.querySelector("#treesCardContainer");
+const cartContainer = document.querySelector("#cartContainer");
 
 // Function to load the category
 const loadCategory = async () => {
@@ -79,25 +96,64 @@ const showTressByCategory = (trees) => {
                 <figure class="h-40">
                     <img
                     src="${tree.image}"
-                    alt="${tree.name}" loading="lazy" />
+                    alt="${tree.name}" />
                 </figure>
                 <div class=" overflow-clip">
                     <h2 class="card-title mt-3">${tree.name}</h2>
                     <p class="truncate text-[#1F2937] text-sm font-medium my-3">${tree.description}</p>
                     <div class="flex justify-between items-center">
                         <div class="badge bg-green-200 text-[#15803D] font-medium">${tree.category}</div>
-                        <p class="font-medium text-[#1F2937]">$<span>${tree.price}</span></p>
+                        <p class="font-medium text-[#1F2937]">$<span class="price">${tree.price}</span></p>
                     </div>
                     <div class="card-actions justify-end mt-3">
-                        <button class="btn bg-[#15803D] hover:bg-[#127737] text-white w-full rounded-full">Add to Cart</button>
+                        <button class="add2CartBtn btn bg-[#15803D] hover:bg-[#127737] text-white w-full rounded-full">Add to Cart</button>
                     </div>
                 </div>  
             </div>  
       `;
         });
+        // Function for cart
+        const addToCartBtn = document.querySelectorAll(".add2CartBtn");
+        addToCartBtn.forEach((cartBtn) => {
+            cartBtn.addEventListener("click", (e) => {
+                const parent = e.target.parentNode.parentNode;
+                const title = parent.querySelector(".card-title").textContent;
+                const price = parent.querySelector(".price").textContent;
+                console.log(title, price);
+                showCart(title, price);
+            });
+        });
     } catch (e) {
         console.log(e);
     }
+};
+
+// Function for add treen into cart
+const showCart = (title, price) => {
+    cartContainer.innerHTML += `
+          <div class="p-2 mb-3  shadow rounded-2xl">
+            <div
+              class="bg-green-100 rounded-2xl flex justify-between items-center px-4 py-3 "
+            >
+              <div class="space-y-1">
+                <h5 class="font-medium">${title}</h5>
+                <span class="text-gray-400 "
+                  ><i
+                    class="fa-solid fa-bangladeshi-taka-sign text-gray-400 text-sm"
+                  ></i
+                  ><span>${price}</span>
+                  <i class="fa-solid fa-xmark text-gray-400 text-[9px]"></i>
+                  1
+                </span>
+              </div>
+              <span
+                ><i
+                  class="removeCart fa-solid fa-xmark text-gray-400 text-sm cursor-pointer"
+                ></i
+              ></span>
+            </div>
+          </div>
+  `;
 };
 
 // Function to load All Trees Category by Default
