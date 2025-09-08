f56a5ad3ce90be4f076b94512a932c6b26ccfa84
mobinkhanweb <mobinkhanweb@gmail.com>
2025-09-09 00:24:48 +0600

    updated js and style

--- script/script.js
+++ script/script.js
@@ -5,8 +5,6 @@
 const mobileNav = document.getElementById("mobileNav");
 const overlay = document.getElementById("overlay");
 
-// Array for cart
-let carts = [];
 const showNav = () => {
     mobileNav.classList.remove("right-[-200px]");
     mobileNav.classList.add("right-0");
@@ -22,7 +20,14 @@ const closeNav = () => {
 const categoriesContainer = document.querySelector("#categoriesContainer");
 const treesCardContainer = document.querySelector("#treesCardContainer");
 const cartContainer = document.querySelector("#cartContainer");
+const mobileCartContainer = document.querySelector("#mobileCartContainer");
+const cartCount = document.querySelector("#cartCount");
 const loader = document.querySelector("#loader");
+const treeDetail = document.querySelector("#treeDetail");
+const modalContainer = document.querySelector("#modalContainer");
+
+// Array for cart
+let carts = [];
 
 // Function to show to loading
 const showLoading = () => {
@@ -46,25 +51,15 @@ const loadCategory = async () => {
 // Function to display categories
 const showCategory = (categories) => {
     //   console.log(categories);
-    categoriesContainer.innerHTML = `<li id="allTrees" class="category-btn text-[#1F2937] hover:bg-green-700 hover:text-white rounded-md p-2 cursor-pointer ">All tress</li>`;
+    categoriesContainer.innerHTML = `<li id="allTrees" class="category-btn border border-green-700  md:border-none text-[#1F2937] hover:bg-green-700 hover:text-white rounded-md p-2 cursor-pointer ">All tress</li>`;
     //   console.log(categories);
     categories.forEach((category) => {
         const categoryName = category.category_name;
         const id = category.id;
         categoriesContainer.innerHTML += `
-            <li id='${id}' class="category-btn text-[#1F2937] hover:bg-green-700 hover:text-white rounded-md p-2 cursor-pointer">${categoryName}</li>
+            <li id='${id}' class="category-btn border border-green-700  md:border-none text-[#1F2937] hover:bg-green-700 hover:text-white rounded-md p-2 cursor-pointer">${categoryName}</li>
     `;
     });
-
-    // load word detail 
-    const loadTreeDetail = async (id) => {
-        const url = `https://openapi.programming-hero.com/api/plant/${id}`;
-        const res = await fetch(url);
-        const details = await res.json();
-        console.log(details);
-    };
-
-
 
     // load Trees category
     categoriesContainer.addEventListener("click", (e) => {
@@ -107,37 +102,44 @@ const showTressByCategory = (trees) => {
         trees.forEach((tree) => {
             //   console.log(tree);
             treesCardContainer.innerHTML += `
-            <div id="${tree.id}" class="card p-4 bg-gray-50 h-fit shadow-sm  hover:bg-white">
-                <figure class="h-40">
+            <div id="${tree.id}" class="card p-4 bg-white h-fit shadow-sm ">
+                <figure class="h-40 rounded-xl">
                     <img
                     src="${tree.image}"
-                    alt="${tree.name}" />
+                    alt="${tree.name}" loading="lazy"/>
                 </figure>
-                <div class="overflow-clip">
-                    <h2 class="card-title mt-3">${tree.name}</h2>
-                    <p class="truncate text-[#1F2937] text-sm font-medium my-3">${tree.description}</p>
+                <div class="overflow-clip space-y-4">
+                    <div class="space-y-3">
+                        <h2 class="card-title mt-3 inline-block">${tree.name}</h2>
+                     <p class="truncate text-[#1F2937] text-sm font-medium my-3">${tree.description}</p> 
+                    </div>
                     <div class="flex justify-between items-center">
-                        <div onClick="loadTreeDetail(${tree.id})" class="badge bg-green-200 text-[#15803D] font-medium">${tree.category}</div>
+                        <div class="badge bg-green-200 text-[#15803D] font-medium">${tree.category}</div>
                         <p class="font-medium text-[#1F2937]">$<span class="price">${tree.price}</span></p>
                     </div>
                     <div class="card-actions justify-end mt-3">
-                        <button class="add2CartBtn btn bg-[#15803D] hover:bg-[#127737] text-white w-full rounded-full">Add to Cart</button>
+                        <button class="btn bg-[#15803D] hover:bg-[#127737] text-white w-full rounded-full">Add to Cart</button>
                     </div>
                 </div>  
             </div>  
       `;
         });
-        // Function for cart
-        const addToCartBtn = document.querySelectorAll(".add2CartBtn");
-        addToCartBtn.forEach((cartBtn) => {
-            cartBtn.addEventListener("click", (e) => {
-                handleCart(e);
-            });
-        });
-    } catch (e) {
-        console.log(e);
+    } catch (error) {
+        console.log(error);
     }
 };
+
+// Event Deligation for All Tree cards:
+// Listent for clicks on the treeCardContainer and check
+// if the target is Add to cart btn or tree title (to open detail modal)
+treesCardContainer.addEventListener("click", (e) => {
+    if (e.target.innerText === "Add to Cart") {
+        handleCart(e);
+    }
+    if (e.target.classList[0] === "card-title") {
+        handleViewModal(e);
+    }
+});
 
 // Function for handle Cart
 const handleCart = (e) => {
@@ -153,17 +155,17 @@ const handleCart = (e) => {
     showCart(carts);
 };
 
-// Function for add treen into cart
+// Function for add tree into cart
 const showCart = (carts) => {
     // alert("added cart")
     const totalPrice = carts.reduce((acc, cur) => acc + Number(cur.price), 0);
-    console.log(totalPrice);
     cartContainer.innerHTML = "";
+    mobileCartContainer.innerHTML = "";
     carts.forEach((cart) => {
         cartContainer.innerHTML += `
             <div id="${cart.id}" class="p-2 mb-2 shadow rounded-2xl">
               <div
-                class="bg-green-100 rounded-2xl flex justify-between items-center px-4 py-3 "
+                class="bg-[#F0FDF4] rounded-2xl flex justify-between items-center px-4 py-3 "
               >
                 <div class="space-y-1">
                   <h5 class="font-medium">${cart.title}</h5>
@@ -185,14 +187,49 @@ const showCart = (carts) => {
               </div>
     `;
     });
-    const div = document.createElement("div");
-    div.innerHTML = `
-        <div class="flex justify-between items-center">
+    carts.forEach((cart) => {
+        mobileCartContainer.innerHTML += `
+            <div id="${cart.id}" class="p-2 mb-2 shadow rounded-2xl">
+              <div
+                class="bg-[#F0FDF4] rounded-2xl flex justify-between items-center px-4 py-3 "
+              >
+                <div class="space-y-1">
+                  <h5 class="font-medium">${cart.title}</h5>
+                  <span class="text-gray-400 "
+                    ><i
+                      class="fa-solid fa-bangladeshi-taka-sign text-gray-400 text-sm"
+                    ></i
+                    ><span>${cart.price}</span>
+                    <i class="fa-solid fa-xmark text-gray-400 text-[9px]"></i>
+                    1
+                  </span>
+                </div>
+                <span
+                  ><i onclick="handleDeleteItem(${cart.id})"
+                    class="removeCart fa-solid fa-xmark text-gray-400 text-sm cursor-pointer"
+                  ></i
+                ></span>
+              </div>
+              </div>
+    `;
+    });
+    const divForPc = document.createElement("div");
+    divForPc.innerHTML = `
+        <div class="flex justify-between items-center mt-4 pt-3 border-t border-gray-300">
             <h4 class="font-medium">Total</h4>
             <p><i class="fa-solid fa-bangladeshi-taka-sign"></i><span class="font-medium">${totalPrice}</span></p>
         </div> 
   `;
-    cartContainer.appendChild(div);
+    cartContainer.appendChild(divForPc);
+    const divForMobile = document.createElement("div");
+    divForMobile.innerHTML = `
+        <div class="flex justify-between items-center mt-4 pt-3 border-t border-gray-300">
+            <h4 class="font-medium">Total</h4>
+            <p><i class="fa-solid fa-bangladeshi-taka-sign"></i><span class="font-medium">${totalPrice}</span></p>
+        </div> 
+  `;
+    mobileCartContainer.appendChild(divForMobile);
+    cartCount.textContent = carts.length;
 };
 
 // Function for delete item from cart
@@ -202,6 +239,34 @@ const handleDeleteItem = (id) => {
     showCart(carts);
 };
 
+// === Functioin for  handle view modal===
+const handleViewModal = async (e) => {
+    const id = e.target.parentNode.parentNode.parentNode.id;
+    const res = await fetch(
+        `https://openapi.programming-hero.com/api/plant/${id}`
+    );
+    const data = await res.json();
+    showTreeDetail(data.plants);
+};
+const showTreeDetail = (plants) => {
+    modalContainer.innerHTML = "";
+    modalContainer.innerHTML += `
+          <h3 roll="btn" class="text-lg font-bold">${plants.name}</h3>
+          <img class="h-72 my-3 w-full object-cover rounded-xl" src="${plants.image}" alt="${plants.name}" />
+          <div class="space-y-1">
+              <p><span class="font-semibold ">Category:</span> ${plants.category}</p>
+              <p><span class="font-semibold ">Price:</span> ${plants.price}</p>
+              <p><span class="font-semibold ">Description:</span> ${plants.description}</p>
+          </div>
+          <div class="modal-action">
+            <form method="dialog">
+              <button class="btn bg-green-700 text-white">Close</button>
+            </form>
+          </div>
+  
+  `;
+    treeDetail.showModal();
+};
 // Function to load All Trees Category by Default
 const loadAllTreesCategory = async () => {
     try {
